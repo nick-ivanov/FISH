0fec73e43932ca86abaf840eafc3f39ec1aa970f
Nick Ivanov <nnrowan@gmail.com>
2016-09-18 20:05:01 +0200

    Fixes in fish.c are made.

--- src/fish.c
+++ src/fish.c
@@ -181,7 +181,8 @@ int main()
 				printf("RUN, LAUNCH, LIST, COPY, REMOVE, CD, SHOW, HELP, QUIT, MKDIR, RMDIR\n\n");
 				printf("To get help on each of those commands, type HELP <command> (i.e. HELP CD)");
 
-				cout << "NOTE that FISH commands are case-insensitive, yet all the arguments" << endl;
+				printf("NOTE that FISH commands are case-insensitive, yet all the arguments\n");
+				
 				cout << "are case-sensitive (except for HELP whose argument is case-insensitive)." << endl;
 				continue;
 			}
