02e81a2ca56a9dde603cc82241d946c3f62e6fba
Nick Ivanov <nnrowan@gmail.com>
2016-09-26 20:05:01 +0200

    Fixes in fish.c are made.

--- src/fish.c
+++ src/fish.c
@@ -200,8 +200,8 @@ int main()
 			}
 
 			if(!strcmp(A[1], "launch")) {
-				cout << "LAUNCH searches a program in PATH directories and executes it." << endl;
-				cout << "FORMAT:" << endl;
+				printf("LAUNCH searches a program in PATH directories and executes it.\n");
+				printf("FORMAT:\n");
 				cout << "\tLAUNCH <program> [<argument1> <argument2> .. <argument9>]" << endl;
 				cout << "EXAMPLE:" << endl;
 				cout << "\tLAUNCH ls -l" << endl;
