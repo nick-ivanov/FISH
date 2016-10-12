040481ee6c3dc6a02e65db4808365bc24bf443d9
Nick Ivanov <nnrowan@gmail.com>
2016-10-12 20:05:01 +0200

    Fixes in fish.c are made.

--- src/fish.c
+++ src/fish.c
@@ -554,7 +554,7 @@ int main()
 			if(mkdir(A[1], 0755) == -1) {
 				fprintf(stderr, "FISH ERROR: Cannot create a directory.\n");
 			} else {
-i				printf("Welcome on board, %s!\n", A[1]);
+				printf("Welcome on board, %s!\n", A[1]);
 			}
 
 			continue;
