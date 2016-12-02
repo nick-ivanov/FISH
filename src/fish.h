56a0f0128b93bbeb60cdd41ee09409f7033a65b9
Nick Ivanov <nnrowan@gmail.com>
2016-12-02 20:05:01 +0200

    Commit ID: ADFE97F6

--- src/fish.h
+++ src/fish.h
@@ -1,3 +1,40 @@
+/*
+FILE NAME:     parse.h
+VERSION:       1.1
+DESCRIPTION:   Fantasticly Incredible Shell (FISH) -- pretty good Unux shell.
+AUTHOR:        Copyright (C) 2015-2016 Nikolay Ivanov (Nick) <nnrowan@gmail.com>
+LICENSE:       GNU General Public License v.3
+               [http://www.gnu.org/licenses/gpl-3.0.en.html]
+
+DOCUMENTATION: Build and run the program; then type HELP to
+                     to see the list of commands:
+                     $ make
+                     $ ./fish
+                     [...] ><(((*> HELP
+*/
+
+
+#define FISH_MAX_CMD_WORDS	64		// Maximum number of words in a command
+#define FISH_MAX_WORD_LEN	256		// Maximum length of a word
+
+
+/* 
+This function reads a line from the keyboard and writes each
+word into an array of strings. The array and its resulting size
+are passed as arguments by reference. The function checks the
+boundaries and ignores any series of spaces wherever they are.
+
+NOTE: This version of the function is not designed to process
+tabs and other whitespace characters.
+
+ARGUMENTS:
+	A:		array of strings the words will be written into;
+
+RETURN VALUE: The number of words read.
+*/
+
+int parse(char** A);
+
 
 /* TODO: Function description */
 void help(int n);
