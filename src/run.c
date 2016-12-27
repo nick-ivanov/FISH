d751f64fa620dec65c480341ade8c44617644326
Nick Ivanov <nnrowan@gmail.com>
2016-12-27 20:05:01 +0200

    Commit ID: D0681F18

--- src/run.c
+++ src/run.c
@@ -6,5 +6,37 @@
 #include "fish.h"
 
 void run(char ** A, int n) {
-	;
+	if(n < 2 || n > 10) {
+		fprintf(stderr, "FISH SYNTAX ERROR: Command RUN takes one to nine arguments.\n");
+		continue;
+	}
+
+	pid_t pid = fork();
+
+	if(pid == -1) {
+		perror("FISH internal error.");
+		continue;
+	} else if (pid == 0) {
+		switch(n) // Yes it looks gloomy, but it is easier than using execv()!
+		{
+			case 2: execl(A[1], A[1], NULL); break;
+			case 3: execl(A[1], A[1], A[2], NULL); break;
+			case 4: execl(A[1], A[1], A[2], A[3], NULL); break;
+			case 5: execl(A[1], A[1], A[2], A[3], A[4], NULL); break;
+			case 6: execl(A[1], A[1], A[2], A[3], A[4], A[5], NULL); break;
+			case 7: execl(A[1], A[1], A[2], A[3], A[4], A[5], A[6], NULL); break;
+			case 8: execl(A[1], A[1], A[2], A[3], A[4], A[5], A[6], A[7], NULL); break;
+			case 9: execl(A[1], A[1], A[2], A[3], A[4], A[5], A[6], A[7], A[5], NULL); break;
+			case 10: execl(A[1], A[1], A[2], A[3], A[4], A[5], A[6], A[7], A[8], A[9], NULL); break;
+			
+			default:
+				fprintf(stderr, "FISH SYNTAX ERROR: Command RUN takes one to eleven arguments.\n");
+				continue;
+		}
+
+		perror("FISH ERROR");
+		exit(1);	// The child is still alive and kickin'.
+	} else {
+		wait(NULL);
+	}
 }
