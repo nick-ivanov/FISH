248ec2ff7d3adfbb929516205ecfbd3b1a3d17a2
Nick Ivanov <nnrowan@gmail.com>
2017-06-02 21:05:01 +0200

    Commit ID: 657E00D6

--- src/list.c
+++ src/list.c
@@ -19,24 +19,30 @@
 #include "fish.h"
 
 void list(char** A, int n) {
-    DIR * dir;
-    struct dirent* entry;
+    if(n == 1) {
+        DIR * dir;
+        struct dirent* entry;
 
-    dir = opendir(getcwd(NULL, 256));
+        dir = opendir(getcwd(NULL, 256));
 
-    if(dir == NULL) {
-        perror("FISH ERROR");
+        if(dir == NULL) {
+            perror("FISH ERROR");
+            closedir(dir);
+            continue;
+        }
+
+        int i = 0;
+        while((entry = readdir(dir)) != NULL) {
+            printf("%s\n", entry->d_name);
+            i++;
+        }
+
+        printf("---\n");
+        printf("TOTAL: %d items.\n", i);
         closedir(dir);
-        continue;
     }
 
-    int i = 0;
-    while((entry = readdir(dir)) != NULL) {
-        printf("%s\n", entry->d_name);
-        i++;
+    if(n == 2) {
+        
     }
-
-    printf("---\n");
-    printf("TOTAL: %d items.\n", i);
-    closedir(dir);
 }
